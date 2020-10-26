e7d7b4ccb6896b7b3954b0fa087f9b5aa7200252
AMORICHIKA <48400718+AMORICHIKA@users.noreply.github.com>
2020-10-26 16:25:25 +0900

    Add files via upload

--- CommandLib/export.h
+++ CommandLib/export.h
@@ -1,1 +1,12 @@
+#pragma once
 
+#if defined( COMMANDLIB_EXPORTS )
+#define _LIB_EXT_CLASS	__declspec( dllexport )
+#define _LIB_EXT_FUNC	__declspec( dllexport )
+#else
+#define _LIB_EXT_CLASS	__declspec( dllimport )
+#define _LIB_EXT_FUNC	__declspec( dllimport )
+#endif
+
+extern "C"
+_LIB_EXT_FUNC	LPWSTR	RunCommand(LPCWSTR lpszCommand);
